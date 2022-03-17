1f6dc4656d5880ada0f248433f79e31690fcfdc0
Nrmeredith <nrmeredith06@gmail.com>
2022-03-17 05:20:50 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,20 +10,79 @@ public class PlayerScript : MonoBehaviour
     public float speed;
     public float jumpforce;
 
+    public GameObject winScreen;
+
+    public GameObject loseScreen;
     public Text score;
 
+    public Text lives;
+
     private int scoreValue = 0;
+
+    private int lifeValue = 3;
+
+    public GameObject level2Spawn;
+
+    bool gameOver;
+
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    public AudioSource musicSource;
+    
+    int i;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        lives.text = lifeValue.ToString();
+        winScreen.SetActive(false);
+        loseScreen.SetActive(false);
+        gameOver = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (gameOver == false)
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.loop = true;
+           
+            if (musicSource.isPlaying == false)
+            {
+                musicSource.Play();
+            }
+            
+
+        }
+        if(gameOver == true)
+        {
+            if (i == 0)
+            {
+                musicSource.Stop();
+                Debug.Log("here1");
+                i++;
+                musicSource.loop = false;
+            }
+            
+            
+            musicSource.clip = musicClipTwo;
+            if (musicSource.isPlaying == false)
+            {
+                if(i == 1)
+                {
+                    musicSource.Play();
+                    i++;
+                }
+                
+            }
+        }
+
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
@@ -32,6 +91,26 @@ public class PlayerScript : MonoBehaviour
         {
             Application.Quit();
         }
+
+        if (scoreValue == 4)
+        {
+            Teleport();
+        }
+        if (scoreValue == 8)
+        {
+            winScreen.SetActive(true);
+            gameOver = true;
+        }
+        if (lifeValue == 0)
+        {
+            loseScreen.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+    private void Teleport()
+    {
+        gameObject.transform.position = level2Spawn.transform.position;
+        Destroy(level2Spawn);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -40,6 +119,14 @@ public class PlayerScript : MonoBehaviour
         {
             scoreValue += 1;
             score.text = scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+        
+
+        if (collision.collider.tag == "Enemy")
+        {
+            lifeValue -= 1;
+            lives.text = lifeValue.ToString();
             Destroy(collision.collider.gameObject);
         }
 
